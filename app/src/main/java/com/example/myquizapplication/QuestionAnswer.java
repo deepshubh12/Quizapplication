699c9cdcc312ef1c1d2d2b5b10c150ec908caf60
Subhadeep <subhadeepkar813@gmail.com>
2023-08-10 20:13:24 +0530

    This is a Quiz Application app.

---
+++ app/src/main/java/com/example/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package com.example.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "Which company owns android?",
+            "Which one is not the programming language?",
+            "who is the CEO of Google?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Satya Nadella","Bill Gates","Sundar Pichai","Elon Musk"}
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Sundar Pichai",
+    };
+}
